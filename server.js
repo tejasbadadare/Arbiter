ed7f39b76609fae7798b304bb4063f6bb7308574
Srivardhan Annam <sannam@ucsd.edu>
2016-10-01 00:33:19 -0700

    We MEAN now bois

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'), app = express()
 
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname ))
 
 app.get('/', function (req, res, next) {
   try {
