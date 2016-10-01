88021f903e96ca6ebffabd78260777d3df8e2c64
tejasbadadare <tejasbadadare@gmail.com>
2016-10-01 12:56:59 -0700

    Mongo database working

--- controllers/addDecision_controller.js
+++ controllers/addDecision_controller.js
@@ -1,43 +1,35 @@
 var mongodb = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb:27017/decisions';
+var url = 'mongodb://localhost:27017/decisions';
 
 exports.view = function(req, res){
 
-    for (var key in req) {
-      console.log("key: " + key);
-    }
+    var userId = "1234";
+    var decision_name = req.body.decision_name;
+    var choice_one = req.body.choice_one;
+    var choice_two = req.body.choice_two;
 
-  console.error("decision_name: " + req.body.decision_name);
+    console.log("decision_name: " + decision_name);
+    console.log("choice_once: " + choice_one);
+    console.log("choice_two: " + choice_two);
 
-  /*var userId = "1234";
-  var decision_name = req.body.decision_name;
-  var choice_one = req.body.choice_one;
-  var choice_two = req.body.choice_two;
+    // Connect to MongoDB and insert new decision_name
+    MongoClient.connect(url, function(err, db) {
+        if (!err) {
+            var collection_decisions = db.collection('decisions');
+            var decisionToAdd = {
+                "decision_name" : decision_name,
+                "choice_one" : choice_one,
+                "choice_two" : choice_two,
+                "userId" : userId
+            };
 
-  console.log("decision_name: " + decision_name);
-  console.log("choice_once: " + choice_one);
-  console.log("choice_two: " + choice_two);
-*/
-}
-  /*
-  MongoClient.connect(url, function(err, db){
-    if(!err){
-      var collection_decisions = db.collection('decisions');
-      var decisionToAdd = {
-        "decision_name" : decision_name,
-        "choice_one" : choice_one,
-        "choice_two" : choice_two,
-        "userId" : userId
-      };
-
-      var ans = collection_decisions.insertOne(
-        decisionToAdd, function(err, resp){
-          console.log(err ? "Shit" : "GOod");
-          db.close();
-          callback(err);
-        });
-      }
+            var ans = collection_decisions.insertOne(decisionToAdd, function(err, resp) {
+                console.log(err ? "DB insert failed." : "DB insert successful.");
+                db.close();
+                console.error(err);
+            });
+        }
     });
-  }
-*/
+}
